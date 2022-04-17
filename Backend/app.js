ab66b4e3bf7663bfee360cb781d9fd6fc0afc1ea
Abhishek-Choubey <96122002+Abhishek-Choubey@users.noreply.github.com>
2022-04-17 12:34:49 +0530

    committed

---
+++ Backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connect = require("./src/config/db");
+const userController = require("./src/controllers/user.controller");
+const {body} = require("express-validator");
+const productController = require("./src/controllers/product.controller")
+const {register, login} = require("./src/controllers/auth.controller");
+const app = express();
+
+app.use(express.json());
+app.use("/user", userController);
+app.use("/product", productController);
+
+app.post("/register",
+body("name").isString().isLength({min : 5}),
+body("email").isEmail(),
+body("password").isLength({min : 6})
+, register)
+
+
+
+app.post("/login", login)
+const start = async()=>{
+    await connect();
+    app.listen(3000, ()=>{
+        console.log("Listening on Port 3000");
+    })
+}
+start();
